228610c324850205b6517d0cadd66fc68557ed4f
Bikesh Chipalu <64654422+Beekaysh@users.noreply.github.com>
2021-03-27 09:42:08 +0545

    Update app1.py

--- app1.py
+++ app1.py
@@ -15,12 +15,16 @@
 
 
 import json
+import difflib
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 def meaning(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:
         return("Sorry, but either the word does not exist, or it's not in our dictionary. Please recheck.\n")
 
