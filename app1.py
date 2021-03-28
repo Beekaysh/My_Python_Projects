dbf34b92c7bd0f3a3c6a83502d7f82dbd909f2f8
Bikesh Chipalu <64654422+Beekaysh@users.noreply.github.com>
2021-03-28 09:17:20 +0545

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,7 +13,6 @@
 #
 # print(translate(word))
 
-
 import json
 import difflib
 from difflib import get_close_matches
@@ -22,6 +21,10 @@ def meaning(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()] #If user enters names of states or cities in lowercase like texas, it will convert it to Texas and check
+    elif w.upper() in data:  # If user enters acronyms like USA
+        return data[w.upper()] 
     elif len(get_close_matches(w,data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter y if yes, or n if no: " % get_close_matches(w,data.keys())[0])
         yn = yn.lower()
